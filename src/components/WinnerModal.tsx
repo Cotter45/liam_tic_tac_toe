bc182455e8fa2754109a1af2664b12fb6a0c9ae2
Sean <sd.cotter45@gmail.com>
2023-09-11 18:58:12 -0400

    css tweak to modal

--- src/components/WinnerModal.tsx
+++ src/components/WinnerModal.tsx
@@ -14,7 +14,7 @@ export const WinnerModal = ({
 }: GameProps) => {
   return (
     <div className="bg-gray-900/90 z-10 min-h-screen w-full absolute top-0 left-0">
-      <div className="w-[500px] h-[250px] rounded-xl bg-[#1f3540] space-y-10 px-6 py-4 mx-auto mt-52 flex items-center justify-center flex-col">
+      <div className="w-[90%] h-[250px] rounded-xl bg-[#1f3540] space-y-10 px-6 py-4 mx-auto mt-52 flex items-center justify-center flex-col">
         <h2 className="flex flex-col items-center justify-center space-y-6 text-2xl md:text-4xl font-bold">
           {winner === "X" ? <XIcon /> : <OIcon />}
           <p className="uppercase text-[#30c4bd]">Takes the Round</p>
@@ -23,7 +23,7 @@ export const WinnerModal = ({
         <div className="flex items-center justify-center space-x-16">
           <button
             onClick={handleQuitGame}
-            className="button px-4 rounded-md py-1 bg-[#a8bdc8] hover:bg-[#718087] hover:ring-4 hover:ring-gray-400"
+            className="button px-8 rounded-md py-1 bg-[#a8bdc8] hover:bg-[#718087] hover:ring-4 hover:ring-gray-400"
           >
             Quit
           </button>
